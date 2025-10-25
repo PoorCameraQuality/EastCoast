251720d92f9b20b8b85a2d26aff3baa1477ee127
shkin <sh.kinney@hotmail.com>
2025-10-25 10:11:51 -0400

    feat: remove promotional content from homepage
    - Remove PromotionalNews component from homepage
    - Test period for promotional banners has ended
    - Clean up homepage layout by removing promotional sidebar and mobile banners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Hero from '@/components/Hero'
 import EventsSection from '@/components/EventsSection'
 import PastEventsSection from '@/components/PastEventsSection'
 import AboutSection from '@/components/AboutSection'
-import PromotionalNews from '@/components/PromotionalNews'
 import { HomepageStructuredData } from '@/components/StructuredData'
 
 export default function Home() {
   return (
     <>
       <HomepageStructuredData />
-      <PromotionalNews />
       <main lang="en">
         <Hero />
         <EventsSection />
